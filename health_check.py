8016da2509e81c34c865bd2094a85e5eaf103a89
laeshin.park <laeshin.park@cdnetworks.co.kr>
2015-04-08 13:39:33 +0900

    add use_httplib(), use_urllib()

--- health_check.py
+++ health_check.py
@@ -1,6 +1,71 @@
+import httplib
+import urllib
+import time
+
+
+def use_httplib():
+    """Use httplib module
+    refer : https://docs.python.org/2/library/httplib.html
+    """
+    print '#' * 30
+    print '# Use httplib #'
+
+    start = time.time()
+    print 'start:', start
+
+    conn = httplib.HTTPConnection('www.stackoverflow.com')
+    conn.request('GET', '/')    # GET, HEAD, POST, ....
+
+    # Should be called after a request is sent to get the response from the server.
+    # Returns an HTTPResponse instance.
+    res = conn.getresponse()
+
+    # netstat -napc | grep :80
+    conn.close()    # Close the connection to the server
+
+    data = res.read()
+    print 'data:', data
+
+    end = time.time()
+    print 'end:', end
+
+    elapsed_time = end - start
+    print 'elapsed_time:', elapsed_time
+
+    print res.status, ':', res.reason
+    print '#' * 30 + '\n\n'
+
+
+def use_urllib():
+    print '#' * 30
+    print '# Use urllib #'
+
+    start = time.time()
+    print 'start:', start
+
+    res = urllib.urlopen("http://www.stackoverflow.com")
+    print dir(res)
+    print res.fileno
+    print dir(res.fileno)
+    print res.fp
+    print dir(res.fp)
+    print res.getcode()
+    print res.headers
+
+
+
+    end = time.time()
+    print 'end:', end
+    diff = end - start
+    print 'diff:', diff
+
+    print '#' * 30 + '\n\n'
 
 
 def main():
+    use_httplib()
+    use_urllib()
+
 
 
 
